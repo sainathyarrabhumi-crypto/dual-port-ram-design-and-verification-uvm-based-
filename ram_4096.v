9c7ff6d3cbacb496019b7b51a61ec8e0fde49b8f
yerrabhumi sainath reddy <sainathyarrabhumi@gmail.com>
2025-09-18 00:10:36 +0530

    Update ram_4096.v

--- ram_4096.v
+++ ram_4096.v
@@ -1,22 +1,3 @@
-/************************************************************************
-  
-Copyright 2019 - Maven Silicon Softech Pvt Ltd.  
-  
-www.maven-silicon.com 
-  
-All Rights Reserved. 
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd. 
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
- 
-Filename:	ram_4096.v   
-  
-Author Name:    Putta Satish
-
-Version:	1.0
-
-************************************************************************/
-
 `define RAM_WIDTH 64
 `define ADDR_SIZE 12
 
